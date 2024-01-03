76e4cf4620b43ff189019bc9b7af1558609cc430
Chris De Asis <138704176+cde-as@users.noreply.github.com>
2024-01-03 09:19:01 -0800

    accepts command line args from user using const breedName

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,25 @@
 const request = require("request");
 
-const catAPI = "https://api.thecatapi.com/v1/breeds/search?q=siberian";
+const breedName = process.argv[2];
+
+if (!breedName) {
+  console.error('Please provide a cat breed.');
+  process.exit(1);
+}
+
+const catAPI = "https://api.thecatapi.com/v1/breeds/search?q=${breedName}";
 
 request(catAPI, (error, response, body) => {
+
   if (error) {
     console.log("Error", error);
   } else if (response.statusCode !== 200) {
     console.error("API request failed:", response.statusCode);
   } else {
     const data = JSON.parse(body);
+    
     console.log(data);
-    console.log(typeof data);
+    console.log(data.description);
   }
 });
 
