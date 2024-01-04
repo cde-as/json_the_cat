1f60c60461d2d3949c81e285174585a89741e403
Chris De Asis <138704176+cde-as@users.noreply.github.com>
2024-01-03 17:30:46 -0800

    fetchBreedDescription appears to be working, need to remove console.logs

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,36 +1,34 @@
 const request = require("request");
 
-const breedName = process.argv[2];
-
-if (!breedName) {
-  console.error('Please provide a cat breed.');
-  process.exit(1);
-}
-
-const catAPI = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
-
-request(catAPI, (error, response, body) => {
-
-  if (error) {
-    console.log("Error", error);
-  } else if (response.statusCode !== 200) {
-    console.error("API request failed:", response.statusCode);
-  } else {
-    const data = JSON.parse(body);
-
-    if (Array.isArray(data) && data.length > 0) {
-      //ensures that the API includes data and is not empty
-      const userEntry = data[0];
-      
-      // Check if the user entry has a description property
-      if (userEntry.description) {
-        console.log(`Description for ${breedName} breed:`, userEntry.description);
-      } else {
-        console.log(`No description found for ${breedName} breed.`);
-      }
+const fetchBreedDescription = function(breedName, callback) {
+  const catAPI = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  
+  request(catAPI, (error, response, body) => {
+    if (error) {
+      console.log("Error", error);
+    } else if (response.statusCode !== 200) {
+      console.error("API request failed:", response.statusCode);
     } else {
-      console.log(`No data found for ${breedName} breed.`);
-    }
-  }
-});
+      const data = JSON.parse(body);
 
+      if (Array.isArray(data) && data.length > 0) {
+        //ensures that the API includes data and is not empty
+        const userEntry = data[0];
+
+        // Check if the user entry has a description property
+        if (userEntry.description) {
+          console.log(
+            `Description for ${breedName} breed:`,
+            userEntry.description
+          );
+        } else {
+          console.log(`No description found for ${breedName} breed.`);
+        }
+      } else {
+        console.log(`No data found for ${breedName} breed.`);
+      }
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
